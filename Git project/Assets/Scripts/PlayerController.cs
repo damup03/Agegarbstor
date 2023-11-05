6bc7e5cb596b42fd01c1b8a54fe6b673b67b836b
damup03 <damup03@gmail.com>
2023-11-06 00:03:11 +0300

    Start's projects's files
    Menu + 2.5D Player Contoller

---
+++ Git project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rigidbody;
+    public float rotationspeed = 10f;
+    public float speed = 2f;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+        
+    }   
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 directionVector = new Vector3(h, 0, v);
+
+        if (directionVector.magnitude > Mathf.Abs(0.05f))
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * 10);
+        animator.SetFloat("speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
+        rigidbody.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
+        
+    }
+}
